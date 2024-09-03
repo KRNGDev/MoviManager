8a935a1e60ceaac5fb5cd14778279f61b0ef4ac1
Alberto Lopez Martinez <krng.taure@gmail.com>
2024-09-03 20:50:35 +0200

    sum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,14 +14,11 @@ import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutlin
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Buscar', url: '/folder/Buscar', icon: 'mail' },
+    { title: 'Mi lista', url: '/folder/Mi Lista', icon: 'paper-plane' },
+    { title: 'Favoritos', url: '/folder/Favoritos', icon: 'heart' }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  public labels = ['Accion', 'Fantasia', 'Terror'];
   constructor() {
     addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
   }
