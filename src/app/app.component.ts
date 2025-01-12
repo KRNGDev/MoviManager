787c2e2afe5a925cca9ef2ae67ec1e7b175208af
KRNGDev <krng.taure@gmail.com>
2025-01-12 16:00:55 +0100

    Sum final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink, IonTitle, IonFooter } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { mailOutline, exit, exitOutline, exitSharp, search, searchCircle, searchOutline, searchSharp, searchCircleOutline, searchCircleSharp, library, libraryOutline, librarySharp, planet, planetOutline, planetSharp, home, homeOutline, homeSharp, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-root',
@@ -18,9 +19,12 @@ export class AppComponent {
     { title: 'Buscar', url: '/pages/buscar', icon: 'search' },
     { title: 'Mi lista', url: '/pages/milista', icon: 'library' },
   ];
-  public labels = ['Accion', 'Fantasia', 'Terror'];
+ 
   constructor() {
     addIcons({ mailOutline, exit, exitOutline, exitSharp, search, searchOutline, searchSharp, searchCircle, searchCircleOutline, searchCircleSharp, library, libraryOutline, librarySharp, planet, planetOutline, planetSharp, home, homeOutline, homeSharp, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+  }
+  salirApp(){
+    App.exitApp();
   }
 
 }
